9c2ee25949700eec648e6ea3f642b9aeab3f30c0
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-13 17:16:58 -0400

    create h1

--- events/click_event.js
+++ events/click_event.js
@@ -1,8 +1,16 @@
 var clickEvent = {
+    eleid,callbacke,
     create_click_event: function(elementID,callback)
     {
         var ele = document.getElementById(`${elementID}`);
         ele.setAttribute("onclick",`${callback}`)
+
+        this.eleid = elementID
+        this.callbacke = callback
+    },
+    get_clicked: function(callback)
+    {
+        
     }
-    
+
 }
---
+++ events/create_element.js
@@ -0,0 +1,7 @@
+var create = {
+    create_header: function(text)
+    {
+        var ele_nme = document.createElement('h1');
+        ele_nme.innerHTML=`${text}`
+    }
+}
